184a1a3ebf408d6972700324b42841864517b0da
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2024-04-22 15:55:52 -0700

    Add test config values.

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,8 @@ config :clear_settle_engine, ClearSettleEngine.Repo,
   database: System.get_env("DB_DATABASE", "clear_settle_engine_dev"),
   hostname: System.get_env("DB_HOST", "localhost"),
   port: System.get_env("DB_PORT", "5432"),
+  ssl: true,
+  ssl_opts: [verify: :verify_none],
   pool_size: String.to_integer(System.get_env("DB_POOL_SIZE", "10"))
 
 config :logger,
