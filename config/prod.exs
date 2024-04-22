bf85b62d1dbea4064a4d541a7718d8531988960e
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2024-04-22 15:26:58 -0700

    Changed config files.

--- config/prod.exs
+++ config/prod.exs
@@ -1,16 +1,9 @@
-import Config
-
-config :clear_settle_engine,
-  ecto_repos: [ClearSettleEngine.Repo]
-
 config :clear_settle_engine, ClearSettleEngine.Repo,
   username: System.get_env("DB_USERNAME", "postgres"),
   password: System.get_env("DB_PASSWORD", ""),
   database: System.get_env("DB_DATABASE", "clear_settle_engine_dev"),
   hostname: System.get_env("DB_HOST", "localhost"),
   port: System.get_env("DB_PORT", "5432"),
+  ssl: true,
+  ssl_opts: [verify: :verify_none],
   pool_size: String.to_integer(System.get_env("DB_POOL_SIZE", "10"))
-
-config :logger,
-  backends: [:console],
-  level: :error
